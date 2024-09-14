72f8889646024958c4c9fc194ce0eb16ebe5546c
yeison34 <jeisonperenguez1998@gmail.com>
2024-09-14 02:48:17 -0500

    csproj

--- PostgradosGestionInformacion/Controllers/Coordinador/CoordinadorController.cs
+++ PostgradosGestionInformacion/Controllers/Coordinador/CoordinadorController.cs
@@ -1,7 +1,7 @@
-﻿using Data.Utilidades;
+﻿
+using Data.Utilidades;
 using Entities.Coordinador;
 using Microsoft.Ajax.Utilities;
-using Negocio.Coordinador;
 using System;
 using System.Collections.Generic;
 using System.IO;
